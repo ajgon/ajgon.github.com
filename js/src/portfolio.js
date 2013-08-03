2145fdfd7642dfaee7766fe808b6b18b410367d1
Igor Rzegocki <ajgon@irgon.com>
2013-08-03 15:49:42 +0200

    Portfolio flipboard finished

--- js/src/portfolio.js
+++ js/src/portfolio.js
@@ -7,12 +7,38 @@ define(['jquery', 'isotope'], function ($) {
     var Portfolio = {
         init: function () {
             this.container = $('#portfolioItems');
-            // this.container.isotope({
-            //     itemSelector: '.item',
-            //     masonry: {
-            //         columnWidth: 160
-            //     }
-            // }); //.isotope('shuffle');
+            this.containerPaddings = parseInt(this.container.css('padding-left'), 10) + parseInt(this.container.css('padding-right'), 10);
+            this.masonryCols = -1;
+            this.container.isotope({
+                itemSelector: '.item',
+                masonry: {
+                    columnWidth: 155
+                },
+                onLayout: this.isotopeOnLayout
+            }).isotope('shuffle');
+
+            $(window).resize(function () {
+                var containerDistance = Portfolio.container.parent().width() - Portfolio.container.outerWidth();
+                if (containerDistance > 155 || containerDistance < 0) {
+                    if (containerDistance < 0) {
+                        containerDistance -= 155;
+                    }
+                    Portfolio.container.width(Portfolio.container.width() + parseInt(containerDistance / 155, 10) * 155);
+                }
+            });
+        },
+        isotopeOnLayout: function ($elems, isotope) {
+            // var containerWidth = Portfolio.container.parent().width(),
+            //     newContainerWidth = parseInt(containerWidth / 160, 10) * 160,
+            //     containerNewMargin = (containerWidth - newContainerWidth) / 2;
+            // console.log(containerWidth, newContainerWidth, containerNewMargin);
+            // Portfolio.container.width(newContainerWidth - Portfolio.containerPaddings);
+            //
+            if (Portfolio.masonryCols !== isotope.masonry.cols) {
+                Portfolio.container.width(isotope.masonry.cols * 155);
+                Portfolio.masonryCols = isotope.masonry.cols;
+            }
+            console.log(isotope.masonry.cols);
         }
     };
 
