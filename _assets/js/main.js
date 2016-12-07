8f5b111017e8b2c7823b8befae4f03eec7137674
Igor Rzegocki <igor@rzegocki.pl>
2016-12-07 13:34:41 +0100

    Updated CV and Open Source projects

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -10,23 +10,6 @@
 (function ($) {
     'use strict';
 
-    $.getJSON('https://twitterproxy.herokuapp.com/?name=ajgon', function (data) {
-        var $node = $('#recent-tweets').empty();
-        var t = 0;
-        var count = 0;
-        for (t = 0; t < 25; t += 1) {
-            if (!data.tweets[t].text.match(/^(?:RT )?@/)) {
-                $node.append('<li>' + data.tweets[t].text.replace(
-                    /(https?:\/\/[^\s]+[^.])/, '<a href="$1" target="_blank">$1</a>'
-                ) + '</li>');
-                count += 1;
-            }
-            if (count >= 3) {
-                break;
-            }
-        }
-    });
-
     $(document).on('click', 'a[data-width][data-height]', function (e) {
         e.preventDefault();
         window.open(
