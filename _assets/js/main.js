7ec07f9bd36b9abbffc7739997e03c8828f6d99a
Igor Rzegocki <igor@rzegocki.pl>
2018-05-21 16:40:27 +0200

    Added disqs check

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -19,16 +19,18 @@
         );
     });
 
-    /*** DISQS ***/
-    /* * * CONFIGURATION VARIABLES * * */
-    var disqus_shortname = 'ajgon';
+    if (document.getElementById('disqus_thread')) {
+        /*** DISQS ***/
+        /* * * CONFIGURATION VARIABLES * * */
+        var disqus_shortname = 'ajgon';
 
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    }
 }(jQuery));
 
 window['GoogleAnalyticsObject'] = 'ga';
