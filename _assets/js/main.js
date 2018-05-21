4b2d26bfe8c08b31388827ab51476858aa4764a1
Igor Rzegocki <igor@rzegocki.pl>
2018-05-21 16:16:36 +0200

    Moved disqs initialization to external file

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -18,6 +18,17 @@
             $(this).data('height')
         );
     });
+
+    /*** DISQS ***/
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'ajgon';
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
 }(jQuery));
 
 window['GoogleAnalyticsObject'] = 'ga';
